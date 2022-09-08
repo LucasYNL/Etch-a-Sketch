0d57d8aacae56c79aefd91386a2da344fde3eb26
LucasYNL <michaelnggg@hotmail.com>
2022-09-07 20:54:36 -0700

    Updated to DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const sketch = document.querySelector('.sketch');
+
+for(let i = 0; i < 16; i++){
+    const row = document.createElement('div');
+    row.classList.add('section');
+    sketch.append(row);
+    for(let x = 0; x < 16; x++){
+        const blocks = document.createElement('div');
+        blocks.classList.add('part');
+        row.append(blocks);
+    }
+};
+
+//////////////////////////////////////////////////////////
+
 const part = document.querySelectorAll('.part');
 
 part.forEach(function (i) {
