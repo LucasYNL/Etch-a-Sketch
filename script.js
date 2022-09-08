578c86be99333d4614f3b9ad8a897f56680c8f70
LucasYNL <michaelnggg@hotmail.com>
2022-09-07 21:33:46 -0700

    user select size created

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ for(let i = 0; i < 16; i++){
     }
 };
 
-//////////////////////////////////////////////////////////
-
 const part = document.querySelectorAll('.part');
 
 part.forEach(function (i) {
@@ -20,3 +18,40 @@ part.forEach(function (i) {
         i.style.backgroundColor = 'red';
     });
 });
+
+//////////////////////////////////////////////////////////
+
+function setSize(){
+    let down = prompt("Enter the number of columns");
+    let across = prompt("Enter the number of rows");
+    const clear = document.getElementById("draw");
+    const body = document.getElementById("main");
+    const sketch = document.createElement('div');
+
+    clear.remove();
+    sketch.classList.add('sketch');
+    sketch.setAttribute('id', 'draw');
+    body.append(sketch);    
+
+    for(let i = 0; i < across; i++){
+        const row = document.createElement('div');
+        row.classList.add('section');
+        sketch.append(row);
+        for(let x = 0; x < down; x++){
+            const blocks = document.createElement('div');
+            blocks.classList.add('part');
+            row.append(blocks);
+        }
+    };
+
+    const part = document.querySelectorAll('.part');
+
+    part.forEach(function (i) {
+        i.addEventListener('mouseover', () => {
+            i.style.backgroundColor = 'red';
+        });
+    });
+    
+}
+
+
